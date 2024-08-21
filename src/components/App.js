d3fd1be5da0c2c4496a176cdce98635650f5ee9c
dipan samadder <Dipansamadder99@gmail.com>
2024-08-21 11:14:29 +0530

    Layout Created Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
+import Home from "../components/pages/Home";
+import "../styles/App.css";
+import Layout from "./layouts/Layout";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
 
